e2a802db712765f6a87a214fa4a61996cd9e2eae
Francisco <matteomejia@github.com>
2020-04-19 20:18:13 -0500

    Feat(btree): Reimplemented insert and find to support changes made during class

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
-#include <math.h>
 #include <vector> 
+#include <math.h>
 
 template <typename T>
 class SS_Traits{
 public:
     typedef T  value_t;
     typedef std::vector<value_t> container_t;
-    typedef std::vector<value_t*> pcontainer_t;
     typedef typename container_t::iterator iterator_t;
   
     class simple_search{
     public:
-        iterator_t operator() (container_t a, pcontainer_t p, value_t v){
-        return p.end();
+        int operator() (container_t a, value_t v){
+            return 0;
         }
     };
 
     class post_order_print{
     public:
-        void operator() (void) {
+        void operator() (void){
             std::cout << "post order" << std::endl;
         }
     };
@@ -33,13 +32,12 @@ class BS_Traits{
 public:
     typedef T  value_t;
     typedef std::vector<value_t> container_t;
-    typedef std::vector<value_t*> pcontainer_t;
     typedef typename container_t::iterator iterator_t;
   
     class binary_search{
     public:
-        iterator_t operator() (container_t a, pcontainer_t p, value_t v){
-            return p.end();
+        int operator() (container_t a, value_t v){
+            return 0;
         }
     };
 
@@ -59,78 +57,99 @@ class BNode {
 public:
     typedef typename T::value_t value_t;
     typedef typename T::container_t container_t;
-    typedef typename T::pcontainer_t pcontainer_t;
+    typedef typename T::iterador_t iterator_t;
+    typedef std::vector< BNode<T,S>* > pcontainer_t;
 
     container_t keys;
-    BNode *ptrs;
+    pcontainer_t ptrs;
     std::size_t  order;
-    int n;
+    std::size_t n;
     bool isLeaf;
 
-    BNode(bool leaf):order(S){
-        keys=container_t(order,0);
-        ptrs=pcontainer_t(order,NULL);
+    BNode(bool leaf):order(S) {
+        keys = container_t(order,0);
+        ptrs = pcontainer_t(order,NULL);
+        n = 0;
         isLeaf = leaf;
     }
 
-    ~BNode(void){}
+    ~BNode(void) {}
 
-    void insertNonfull (int val) {
-        int i = keys.size() - 1;
+    bool search(int val) {
+        int i = 0;
+        while (i < n && val > keys[i]) {
+            i++;
+        }
+
+        if (keys[i] == val) {
+            return true;
+        }
+
+        if (isLeaf) {
+            return false;
+        }
+
+        return ptrs[i]->search(val);
+    }
+
+    void insertNonFull(T val) {
+        int i = n - 1;
 
         if (isLeaf) {
             while (i >= 0 && keys[i] > val) {
-                keys[i+1] = keys[i];
+                keys[i + 1] = keys[i];
                 i--;
             }
 
-            keys[i+1] = val;
+            keys[i + 1] = val;
             n += 1;
         } else {
             while (i >= 0 && keys[i] > val) {
                 i--;
             }
 
-            if (n == 2*order-1) {
-                splitChild(i+1, ptrs[i+1]);
+            if (ptrs[i + 1]->n == order) {
+                splitChild(i + 1, ptrs[i + 1]);
 
-                if (keys[i+1] < val) {
+                if (keys[i + 1] < val) {
                     i++;
                 }
             }
-            ptrs[i+1]->insertNonfull(val);
+
+            ptrs[i + 1]->insertNonFull(val);
         }
     }
 
-    void splitChild (int i, BNode *t) {
-        BNode<T,S> *b = new BNode<T,S>(t->isLeaf);
-        b->n = order - 1;
+    void splitChild(int i, BNode<T,S> *b) {
+        BNode<T,S> *x = new BNode<T,S>(b->isLeaf);
+        x->n = std::ceil(order/2) - 1;
 
-        for (int j = 0; j < order-1; j++) {
-            b->keys[j] = t->keys[j+order];
+        for (int j = 0; j < std::ceil(order/2) - 1; j++) {
+            x->keys[j] = b->keys[j + x->n];
         }
 
-        if (!t->isLeaf) {
-            for (int j = 0; j < order; j++) {
-                b->ptrs[j] = t->ptrs[j+order];
+        if (!b->isLeaf) {
+            for (int j = 0; j < std::ceil(order/2); j++) {
+                x->ptrs[j] = b->ptrs[j + x->n];
             }
         }
 
-        t->n = order - 1;
+        b->n = std::ceil(order/2) - 1;
 
-        for (int j = n; j >= i+1; j--) {
-            ptrs[j+1] = ptrs[j];
+        for (int j = n; j >= i + 1; j--) {
+            ptrs[j + 1] = ptrs[j]; 
         }
 
-        ptrs[i+1] = b;
+        ptrs[i + 1] = x;
 
-        for (int j = n-1; j >= i; j--) {
-            keys[j+1] = keys[j];
+        for (int j = n - 1; j >= i; j--) {
+            keys[j + 1] = keys[j];
         }
 
-        keys[i] = t->keys[order-1];
+        keys[i] = b->keys[std::ceil(order/2) - 1];
 
         n += 1;
+            
     }
 };
 
@@ -138,6 +157,7 @@ template <typename T, int S>
 class BTree {
 public: 
     typedef typename T::value_t value_t;
+    typedef typename T::container_t container_t;
     typedef typename T::functor_t functor_t;
     typedef typename T::print_t print_t;
 
@@ -145,70 +165,55 @@ public:
     print_t print;
     functor_t search;
 
-    BTree(void):root(NULL) {
+    BTree(void):root(NULL) {}
 
-    }
+    ~BTree(void){}
 
-    ~BTree(void) {}
-
-    void insert(const value_t& val = 0) {
+    void insert(const value_t& val = 0){
         // TODO :: INSERT
         if (root == NULL) {
             root = new BNode<T,S>(true);
             root->keys[0] = val;
-            root->n = 1;
+            root->n += 1;
         } else {
-            if (root->n == 2*S-1) {
-                BNode<T,S> *t = new BNode<T,S>(false);
-                t->ptrs[0] = root;
-                t->splitChild(0, root);
+            if (root->n == root->order) {
+                BNode<T,S> *b = new BNode<T,S>(false);
+
+                b->ptrs[0] = root;
+                b->splitChild(0, root);
 
                 int i = 0;
-                if (t->keys[0] < val) {
+                if (b->keys[0] < val) {
                     i++;
                 }
-                t->ptrs[i]->insertNonfull(val);
 
-                root = t;
-            } else {
-                root->insertNonfull(val);
+                b->ptrs[i]->insertNonFull(val);
             }
         }
     }
 
-    bool find(BNode<T,S> *bn, const value_t val = 0) const {
-        // TODO :: SEARCH
-        // search(x); inside each page
-        int i = 0;
-        while (i < bn->n && val > bn->keys[i]) {
-            i++;
-        }
-
-        if (bn->keys[i] == val) {
-            return true;
-        }
-
-        if (bn->isLeaf) {
+    bool find(const value_t = 0) const{
+        if (root == NULL) {
             return false;
+        } else {
+            return root->search(val);
         }
-
-        return find(bn->ptrs[i], val);
     }
 
     template <typename _T, int _S>
-    friend std::ostream& operator<< (std::ostream& out, BTree<_T,_S> tree){
+    friend std::ostream& operator<<(std::ostream& out, BTree<_T,_S> tree){
         tree.print();// (out)
         // IN PRE POST LEVEL ORDER
         return out;
     }
+
 };
 
 int main() {
     typedef BS_Traits<int> btrait_t;
     BTree<btrait_t,4> tree;
-    tree.find(tree.root, 10);
+    tree.find(10);
     std::cout<<tree<< std::endl;
-
 
     typedef SS_Traits<float> strait_t;
     BTree<strait_t,10> stree; 
